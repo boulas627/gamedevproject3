13d81b8ef877cfb9d31cd5d581fa87690d139474
YINJIE WU <2721541713@qq.com>
2018-11-02 15:50:56 -0400

    Created some scripts

---
+++ gamedevproject3/Assets/Scripts/JournalButton.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class JournalButton : MonoBehaviour {
+    public Button m_YourFirstButton;
+    private JournalPanel journalPanel;
+    // Use this for initialization
+    void Start () {
+        m_YourFirstButton.onClick.AddListener(TaskOnClick);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void TaskOnClick()
+    {
+
+    }
+}
