3093c9973b450c9a76cf2868523a5243e48051d5
Ehren <omegathesigma@gmail.com>
2018-11-09 01:20:36 -0500

    commit before pull

--- gamedevproject3/Assets/ImageFade.cs
+++ gamedevproject3/Assets/ImageFade.cs
@@ -25,10 +25,10 @@ public class ImageFade : MonoBehaviour
         if (fadeAway)
         {
             // loop over 1 second backwards
-            for (float i = 1; i >= 0; i -= Time.deltaTime)
+            for (float i = 3; i >= 0; i -= Time.deltaTime)
             {
                 // set color with i as alpha
-                img.color = new Color(1, 1, 1, i);
+                img.color = new Color(1, 1, 1, i/3);
                 yield return null;
             }
             img.enabled = false;
@@ -38,10 +38,10 @@ public class ImageFade : MonoBehaviour
         {
             img.enabled = true;
             // loop over 1 second
-            for (float i = 0; i <= 1; i += Time.deltaTime)
+            for (float i = 3; i <= 1; i += Time.deltaTime)
             {
                 // set color with i as alpha
-                img.color = new Color(1, 1, 1, i);
+                img.color = new Color(1, 1, 1, i/3);
                 yield return null;
             }
             SceneManager.LoadScene("EndScene");
