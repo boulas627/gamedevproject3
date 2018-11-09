17cf7c996550beca6f04b5c201b46379a456fd7a
YINJIE WU <2721541713@qq.com>
2018-11-08 23:07:05 -0500

    Finished the journal part

--- gamedevproject3/Assets/Scripts/JournalPanel.cs
+++ gamedevproject3/Assets/Scripts/JournalPanel.cs
@@ -11,6 +11,18 @@ public class JournalPanel : MonoBehaviour {
     public bool Minecraft_Poster = false;
     public bool Entry_44 = false;
     public bool Letter_from_Aurora = false;
+    public GameObject Pressed_tomato_leaf_O;
+    public GameObject Entry_36_O;
+    public GameObject Entry_30_O;
+    public GameObject Minecraft_Poster_O;
+    public GameObject Entry_44_O;
+    public GameObject Letter_from_Aurora_O;
+    public GameObject Pressed_tomato_leaf_H;
+    public GameObject Entry_36_H;
+    public GameObject Entry_30_H;
+    public GameObject Minecraft_Poster_H;
+    public GameObject Entry_44_H;
+    public GameObject Letter_from_Aurora_H;
 
     void Awake()
     {
@@ -23,6 +35,35 @@ public class JournalPanel : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+		if(Pressed_tomato_leaf == true && Pressed_tomato_leaf_O.activeSelf == false)
+        {
+            Pressed_tomato_leaf_O.SetActive(true);
+            Pressed_tomato_leaf_H.SetActive(false);
+        }
+        if (Entry_36 == true && Entry_36_O.activeSelf == false)
+        {
+            Entry_36_O.SetActive(true);
+            Entry_36_H.SetActive(false);
+        }
+        if (Entry_30 == true && Entry_30_O.activeSelf == false)
+        {
+            Entry_30_O.SetActive(true);
+            Entry_30_H.SetActive(false);
+        }
+        if (Minecraft_Poster == true && Minecraft_Poster_O.activeSelf == false)
+        {
+            Minecraft_Poster_O.SetActive(true);
+            Minecraft_Poster_H.SetActive(false);
+        }
+        if (Entry_44 == true && Entry_44_O.activeSelf == false)
+        {
+            Entry_44_O.SetActive(true);
+            Entry_44_H.SetActive(false);
+        }
+        if (Letter_from_Aurora == true && Letter_from_Aurora_O.activeSelf == false)
+        {
+            Letter_from_Aurora_O.SetActive(true);
+            Letter_from_Aurora_H.SetActive(false);
+        }
+    }
 }
